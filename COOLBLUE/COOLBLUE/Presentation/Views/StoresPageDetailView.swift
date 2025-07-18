850fd052f95a6b2e596623ba320e77bfa1520124
Clinton Davelaar <c.davelaar@brightfish.nl>
2025-07-18 23:03:56 +0200

    Implement StoresPageView, StoresPageDetailView

---
+++ COOLBLUE/COOLBLUE/Presentation/Views/StoresPageDetailView.swift
@@ -0,0 +1,14 @@
+//
+//  StoresPageDetailView.swift
+//  COOLBLUE
+//
+//  Created by Clinton on 16/07/2025.
+//
+
+import SwiftUI
+
+struct StoresPageDetailView: View {
+    var body: some View {
+        Text("Details Page WIP")
+    }
+}
---
+++ COOLBLUE/COOLBLUE/Presentation/Views/StoresPageView.swift
@@ -0,0 +1,57 @@
+//
+//  StoresPageView.swift
+//  COOLBLUE
+//
+//  Created by Clinton on 16/07/2025.
+//
+
+import SwiftUI
+
+struct StoresPageView: View {
+
+    @State private var viewModel: StoresPageViewModel
+    @State private var coordinator: StoresPageCoordinator
+
+    init() {
+        _viewModel = State(wrappedValue: DIContainer.shared.container.resolve(StoresPageViewModel.self)!)
+        _coordinator = State(wrappedValue: DIContainer.shared.container.resolve(StoresPageCoordinator.self)!)
+    }
+
+    var body: some View {
+        VStack {
+            Toggle("On location", isOn: $viewModel.sortByLocation)
+                .padding(.horizontal)
+            Toggle("On opening hours", isOn: $viewModel.sortByOpeningHours)
+                .padding(.horizontal)
+            ScrollView {
+                LazyVStack {
+                    if let errorMessage = viewModel.errorMessage {
+                        Text(errorMessage)
+                    } else if viewModel.isLoading {
+                        Text("Loading...")
+                    } else if viewModel.stores.isEmpty && viewModel.sortedStores.isEmpty {
+                        Text("No stores found")
+                    } else {
+                        let storesToDisplay = viewModel.sortedStores.isEmpty ? viewModel.stores : viewModel.sortedStores
+                        ForEach(storesToDisplay, id: \.self) { store in
+                            StoreCard(store: store) {
+                                coordinator.navigate(to: .storeDetail(store))
+                            }
+                        }
+                    }
+                }
+                .padding()
+            }
+            .refreshable {
+                Task {
+                    await viewModel.fetchStores(forcedRefresh: true)
+                }
+            }
+        }
+        .task {
+            if viewModel.sortedStores.isEmpty {
+                await viewModel.fetchStores()
+            }
+        }
+    }
+}
